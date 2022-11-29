7fbae5af4c393efbd7d66628141f205b90d9e4f8
bruno <brunofay1@hotmail.com>
2022-11-29 16:24:51 -0300

    NEW add nookies and save info in cookies storage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,21 @@
 import { createContext, ReactNode, useState } from "react";
 import { Api } from "../libs/axios";
+import {setCookie} from 'nookies'
+import { useRouter } from "next/router";
 
 type AuthSignIn = {
    email:string
    password:string
 }
 
+type User = {
+  email:string
+  permissions:string[]
+  roles:string[]
+}
+
 type AuthContext={
+  user:User
   signIn:(session:AuthSignIn)=>Promise<void>
   isAuthenticated:boolean
 }
@@ -14,21 +23,42 @@ type AuthContext={
 export const AuthContext = createContext({} as AuthContext)
 
 export function AuthProvider({children}:{children:ReactNode}){
-  const [isAuthenticated,setIsAuthenticated] = useState<boolean>(false)
-
-
+  const [user,setUser]=useState<User>()
+  const isAuthenticated = !!user
+  const router = useRouter()
  async function signIn({email,password}:AuthSignIn){
     console.log({email,password})
     try {
-      const response = await Api.post('sessions',{email,password})
-      console.log(response.data);
 
+      const response = await Api.post('sessions',{email,password})
+      const {
+        token,
+        refreshToken,
+        permissions,
+        roles
+       } = response.data
+
+      setCookie(undefined,'nextauth.token',token,{
+        maxAge: 60*60*24*30 , // 30 days
+        path:'/'
+      })
+
+      setCookie(undefined,'nextauth.refreshtoken',refreshToken,{
+        maxAge: 60*60*24*30 , // 30 days
+        path:'/'
+      })
+      setUser({
+        email,
+        permissions,
+        roles
+       })
+      router.push('/dashboard')
     } catch (error) {
       console.log(error)
     }
   }
 
-  const valueToProvide={signIn,isAuthenticated}
+  const valueToProvide={signIn,isAuthenticated,user}
 
   return (
           <AuthContext.Provider value={valueToProvide}>
